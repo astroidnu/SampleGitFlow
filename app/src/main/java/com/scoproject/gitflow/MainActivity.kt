ae015df5ef56c10565823ba157920e9a2da73b8a
Ibnu Muzzakkir <ibnu.muzzakkir@amarbank.co.id>
2018-11-27 12:15:06 +0700

    Add space

--- app/src/main/java/com/scoproject/gitflow/MainActivity.kt
+++ app/src/main/java/com/scoproject/gitflow/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.scoproject.gitflow
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import com.scoproject.gitflow.feature.login.LoginActivity
 import com.scoproject.gitflow.register.RegisterActivity
@@ -23,6 +23,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         btnRegister.setOnClickListener {
+
             val intentRegister = Intent(this, RegisterActivity::class.java)
             startActivity(intentRegister)
         }
