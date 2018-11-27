93b781c805f60532e5015a3940c0ac8e3abf7803
Ibnu Muzzakkir <ibnu.muzzakkir@amarbank.co.id>
2018-11-27 11:46:27 +0700

    Add register activty and adjust main activity layout

--- app/src/main/java/com/scoproject/gitflow/MainActivity.kt
+++ app/src/main/java/com/scoproject/gitflow/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.scoproject.gitflow
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.scoproject.gitflow.register.RegisterActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        val btnRegister = findViewById<Button>(R.id.btnRegister)
+
+        btnLogin.setOnClickListener {
+            //TODO Navigate to login activity
+        }
+
+        btnRegister.setOnClickListener {
+            val intentRegister = Intent(this, RegisterActivity::class.java)
+            startActivity(intentRegister)
+        }
     }
 }
---
+++ app/src/main/java/com/scoproject/gitflow/register/RegisterActivity.kt
@@ -0,0 +1,17 @@
+package com.scoproject.gitflow.register
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.scoproject.gitflow.R
+
+/**
+ * Created by ibnumuzzakkir on 27/11/18.
+ * Mobile Engineer
+ */
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
