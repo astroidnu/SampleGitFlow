9d85ed05f508a1baf1ca7e2c8976d0bf1edf96fb
Ibnu Muzzakkir <ibnu.muzzakkir@amarbank.co.id>
2018-11-27 11:31:00 +0700

    Add Login Feature layout and activity file

---
+++ app/src/main/java/com/scoproject/gitflow/feature/login/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.scoproject.gitflow.feature.login
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by ibnumuzzakkir on 27/11/18.
+ * Mobile Engineer
+ */
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
